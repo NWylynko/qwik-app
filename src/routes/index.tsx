0cce4a561fbfbb5bb3152a8b99fd67c8ea2cebae
Nick Wylynko <nick@wylynko.com>
2023-06-18 17:18:33 +0800

    remove some stuff

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,20 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
-import Counter from '~/components/starter/counter/counter';
-import Hero from '~/components/starter/hero/hero';
-import Infobox from '~/components/starter/infobox/infobox';
-import Starter from '~/components/starter/next-steps/next-steps';
-
 export default component$(() => {
   return (
     <>
      <div class="flex justify-center flex-col items-center h-screen">
-      <h1 class="text-5xl font-bold translate-y-[75px] translate-x-[-200px] drop-shadow-lg">Today Lets</h1>
-      <img src="https://test.cdn.copy-travel.com/image/activity-8.jpeg" />
-      <h1 class="text-5xl font-bold translate-y-[-75px] translate-x-[200px] drop-shadow-lg">Go to Paris</h1>
+      <h1 class="text-white text-5xl font-bold translate-y-[75px] translate-x-[-200px] drop-shadow-lg">Today Lets</h1>
+      <img height={780} width={520} src="https://test.cdn.copy-travel.com/image/activity-8.jpeg" />
+      <h1 class="text-white text-5xl font-bold translate-y-[-75px] translate-x-[200px] drop-shadow-lg">Go to Paris</h1>
      </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Welcome to Paris',
   meta: [
     {
       name: 'description',
